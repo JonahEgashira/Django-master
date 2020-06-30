45e87155daaec7657f181b10711e68cb730a255f
Jonah Egashira <jonahega@gmail.com>
2020-06-30 18:05:53 +0900

    Initial Commit

---
+++ message/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import View
+
+class MessageView(View):
+    def get(self, request):
+        #return HttpResponse("Hello, Django!")
+        return render(request, 'index.html')
+
+    def post(self, request):
+        return redirect('/')
